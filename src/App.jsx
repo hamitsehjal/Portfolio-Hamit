397e587bb31dd18a9dfd0775f0b35bd0b165b5a0
Hamit Sehjal <73131093+hamitsehjal@users.noreply.github.com>
2023-07-27 21:21:38 +0000

    Update personal Information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Hamit Sehjal",
+  title: "Software Engineer",
+  email: "hamitsehjal26@gmail.com",
+  gitHub: "hamitsehjal",
+  instagram: "hamit_sehjal/",
+  linkedIn: "hamitsehjal",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "SehjalHamit",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
